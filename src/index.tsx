430ced987a3961f4d9f73e7f9f4cc9cca045534c
MORALES DURAN SEBASTIAN ALEJANDRO <semorale@pichincha.com>
2022-09-30 13:17:57 -0500

    q3 test

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import reportWebVitals from './reportWebVitals'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 import './index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
