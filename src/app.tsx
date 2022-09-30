33829c97f94343cc25b36da91337183fba47dbee
Miguel R <miguel.rosa@devsu.com>
2022-09-30 12:23:10 -0500

    removed leftover icons

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,4 @@
 import './app.scss'
-import DeleteIcon from './assets/delete-icon.svg'
-import EditIcon from './assets/edit-icon.svg'
-import CloseIcon from './assets/close-icon.svg'
 import { APP_CONSTANTS } from './constants/app-constants'
 import { Header } from './components/organisms/header/header'
 
@@ -9,12 +6,6 @@ function App() {
   return (
     <div className="app">
       <Header title={APP_CONSTANTS.APP_TITLE} />
-      <div></div>
-      <div>
-        <img src={DeleteIcon} alt="delete-icon" />
-        <img src={EditIcon} alt="edit-icon" />
-        <img src={CloseIcon} alt="close-icon" />
-      </div>
     </div>
   )
 }
